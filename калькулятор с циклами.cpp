45e8d34fc52aa700bec930b0bd4b595761ccaf15
tanyabuyko99 <32450655+tanyabuyko99@users.noreply.github.com>
2017-10-29 23:05:17 +0300

    Add files via upload

---
+++ калькулятор с циклами.cpp
@@ -0,0 +1,48 @@
+// ConsoleApplication21.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a;
+	double b;
+	int s;
+	int m = 5;
+	int o;
+	while (m > 0) {
+		cout << "Введите 1-ое число: " << endl;
+		cin >> a;
+
+		cout << "Введите 2-ое число: " << endl;
+		cin >> b;
+
+		cout << "Выберите операцию 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление: " << endl;
+		cin >> s;
+
+		if (s == 1) {
+			cout << "a + b = " << a + b << endl << endl;
+		}
+		if (s == 2) {
+			cout << "a - b = " << a - b << endl << endl;
+		}
+		if (s == 3) {
+			cout << "a * b = " << a * b << endl << endl;
+		}
+		if (s == 4) {
+			cout << "a / b = " << a / b << endl << endl;
+		}
+
+		cout << "Введите 9 для продолжения, 0 - для выхода" << endl << endl;
+		cin >> o;
+
+		if (o == 0) {
+			m = 0;
+		}
+		else if (o == 9) {
+			m = 5;
+		}
+	}
+	return 0;
+}
